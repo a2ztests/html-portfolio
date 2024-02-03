d5096c3ce1afca67384e58b61f346fa8cf5eb7b0
a2ztests <39512594+a2ztests@users.noreply.github.com>
2024-02-03 12:29:46 +0200

    Amir portfolio

---
+++ public/skills.py
@@ -0,0 +1,21 @@
+if Skills in ['Python'
+              , 'Big data'
+              , 'Spark'
+              , 'Cloudera'
+              , 'Hadoop'
+              , 'AWS services'
+              , 'Google cloud services'
+              , 'SQL Server'
+              , 'Oracle'
+              , 'DB2'
+              , 'Mongo DB'
+              , 'HBase'
+              , 'SSIS'
+              , 'SSRS'
+              , 'Talend'
+              , 'Tableau'
+              , 'PowerBI'
+              , 'Qlik sense']:
+    Knowledge = True
+Else:
+    Want_to_learn = True    
